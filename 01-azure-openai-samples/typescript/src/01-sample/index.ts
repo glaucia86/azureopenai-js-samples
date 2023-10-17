fc0054063802bedc7f9af02a3f0133bbfd2b7c66
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-10-17 16:50:54 -0300

    chore: initial commit for typescript sample

--- 01-azure-openai-samples/typescript/src/01-sample/index.ts
+++
@@ -1,6 +0,0 @@
-function getPhrase(phrase: string) {
-  return `Hello, ${phrase}!`;
-};
-
-const phrase = getPhrase('Microsoft Reactor!');
-console.log(phrase);
---
+++ 01-azure-openai-samples/typescript/src/01-sample/openAi.ts
@@ -0,0 +1,7 @@
+/**
+ * file: src/01-sample/openAi.ts
+ * description: file responsible for create the first sample of OpenAI API.
+ * data: 10/17/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
