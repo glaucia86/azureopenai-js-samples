fa44a16241d47f5a13f1ef9c33ac0386f7225948
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-10-23 22:17:16 -0300

    chore: create a chat completion version (Node.js)

---
+++ 01-azure-openai-samples/javascript/02-chat-completion/src/index.js
@@ -0,0 +1,52 @@
+/**
+ * file: src/index.js
+ * description: file responsible for run the code sample chat completion
+ * data: 10/23/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const endpoint = process.env.AZURE_OPENAI_ENDPOINT || '';
+const azureApiKey = process.env.AZURE_OPENAI_KEY || '';
+
+const messages = [
+  {
+    role: 'system',
+    content: "You're a helpul assistant. You will talk like a pirate.",
+  },
+  {
+    role: 'user',
+    content: 'Can you help me?',
+  },
+  {
+    role: 'assistant',
+    content: 'Arr! Of course, me hearty! What can I do for ye?',
+  },
+  {
+    role: 'user',
+    content: "What's the Brazilian currency?",
+  },
+];
+
+async function main() {
+  console.log('== Chat Completions Sample ==');
+
+  const client = new OpenAIClient(
+    endpoint,
+    new AzureKeyCredential(azureApiKey)
+  );
+  const deploymentName = 'deployment-name-completion';
+  const result = await client.getChatCompletions(deploymentName, messages);
+
+  for (const choice of result.choices) {
+    console.log(choice.message);
+  }
+}
+
+main().catch((error) => {
+  console.error('The sample encountered an error:', error);
+});
