b2f52c52fe3b68d04102ba625a57d9979b87012d
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-12 19:03:14 -0300

    chore: Add TypeScript configuration and environment files

---
+++ 04-langchain-js-samples/streaming-sample/src/main.ts
@@ -0,0 +1,41 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const model = new ChatOpenAI({
+  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY || "",
+  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION || "",
+  azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME || "",
+  azureOpenAIApiEmbeddingsDeploymentName: process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME || "",
+  azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME || "",
+});
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are an expert at picking company names."],
+  ["human", "What are three good names for a company that makes {product}?"]
+]);
+
+const outputParser = new StringOutputParser();
+
+const nameGenerationChain = RunnableSequence.from([
+  prompt,
+  model,
+  outputParser
+]);
+
+async function main() {
+  const stream = await nameGenerationChain.stream({
+    product: "really cool robots",
+  });
+
+  //Consume the stream
+  for await (const output of stream) {
+    console.log(output);
+  }
+}
+
+main();
