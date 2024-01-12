daf93bdb8ea2ca1ea75dba67bb11aace4e5d8ce4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-12 18:50:10 -0300

    chore: Add TypeScript configuration, environment variables, package.json, and main.ts file for a simple application using Azure OpenAI and TypeScript

---
+++ 04-langchain-js-samples/runnable-sequence-sample/src/main.ts
@@ -0,0 +1,39 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import * as dotenv from "dotenv";
+
+
+dotenv.config();
+
+const model = new ChatOpenAI({
+  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY || "",
+  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION || "",
+  azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME || "",
+  azureOpenAIApiEmbeddingsDeploymentName: process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME || "",
+  azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME || "",
+});
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are an expert at picking company names."],
+  ["human", "What are three good names for a company that makes {product}?"]
+]);
+
+const outputParser = new StringOutputParser();
+
+const nameGenerationChain = RunnableSequence.from([
+  prompt,
+  model,
+  outputParser
+]);
+
+async function main() {
+  const result = await nameGenerationChain.invoke({
+    product: "fancy cookies",
+  });
+
+  console.log(result);
+}
+
+main();
